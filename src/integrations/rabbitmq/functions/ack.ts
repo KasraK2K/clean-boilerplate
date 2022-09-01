9c72f44a77a52cf556b7ba14ccbe638e0c364d95
Kasra Karami <Kasra.Karami.Work@gmail.com>
2022-09-01 20:14:35 +0430

    create producer function

---
+++ src/integrations/rabbitmq/functions/ack.ts
@@ -0,0 +1,27 @@
+import amqp from "amqplib/callback_api"
+import { nextTick } from "process"
+
+const uri = String(process.env.RBBITMQ_URI)
+
+export const producer = (message: Record<string, any>, queue_name: string) => {
+  amqp.connect(uri, (error0, connection) => {
+    if (error0) throw error0
+
+    connection.createChannel((error1, channel) => {
+      if (error1) throw error1
+
+      /**
+       * @argument {durable} This makes sure the queue is declared before attempting to consume from it
+       */
+      channel.assertQueue(queue_name, { durable: true })
+
+      /**
+       * @argument {persistent} Persistent messages will be written to disk as soon as they reach the queue,
+       * while transient messages will be written to disk only so that they can be evicted from memory while under memory pressure
+       */
+      channel.sendToQueue(queue_name, Buffer.from(JSON.stringify(message)), { persistent: true })
+    })
+
+    setTimeout(() => connection.close(), 500)
+  })
+}
