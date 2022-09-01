ac7850a410055c3bd66c70e563a41a9af0e78d45
Kasra Karami <Kasra.Karami.Work@gmail.com>
2022-09-01 22:46:34 +0430

    aadd bullmq

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,45 @@ import { IApplicationConfig } from "./../config/config.interface"
 import config from "config"
 import { getUserInformation } from "./common/helpers/information.helper"
 
+/* -------------------------------------------------------------------------- */
+/*                                   BullMQ                                   */
+/* -------------------------------------------------------------------------- */
+import { Job, Queue, Worker } from "bullmq"
+import IORedis, { RedisOptions } from "ioredis"
+
+const REDIS_OPTIONS: RedisOptions = {
+  host: "localhost",
+  port: 6379,
+  showFriendlyErrorStack: true,
+  enableReadyCheck: true,
+  maxRetriesPerRequest: null,
+}
+
+const connection = new IORedis(REDIS_OPTIONS)
+
+// Reuse the ioredis instance
+const queue = new Queue("queueName", { connection })
+queue.add("jobName", { name: "Kasra", age: Math.round(Math.random() * 100) })
+
+const worker = new Worker(
+  "queueName",
+  async (job: Job) => {
+    console.log(job.data)
+  },
+  {
+    connection,
+  }
+)
+
+worker.on("completed", (job) => {
+  console.log(`${job.id} has completed!`)
+})
+
+worker.on("failed", (job, err) => {
+  console.log(`${job.id} has failed with ${err.message}`)
+})
+/* -------------------------------------------------------------------------- */
+
 const appConfig: IApplicationConfig = config.get("application")
 const port: number = Number(process.env.PORT) || appConfig.port
 
