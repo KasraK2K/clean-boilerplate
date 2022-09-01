48c6f333d7c4cbd901a84155de7baca9d53eb0bb
Kasra Karami <Kasra.Karami.Work@gmail.com>
2022-09-01 20:41:27 +0430

    fix rabbitmq intgration

--- src/integrations/rabbitmq/README.md
+++ src/integrations/rabbitmq/README.md
@@ -1,13 +1,21 @@
 ```typescript
 /* -------------------------------------------------------------------------- */
-/*                                  RabbitMQ                                  */
+/*                                  Consumer                                  */
 /* -------------------------------------------------------------------------- */
-// import rabbitMQ from "./integrations/rabbitmq"
-// const callback = async (message: Record<string, any>) => {
-//   console.log(`this is callback message: ${message.text}`)
-// }
-// const queue_name = "rabbitmq_starter_queue"
-// rabbitMQ.ack.consumer(callback, queue_name)
-// rabbitMQ.ack.producer({ text: "Hello, world!" }, queue_name)
+import rabbitMQ from "integrations/rabbitmq"
+const callback = async (message: Record<string, any>) => console.log(`this is callback message: ${message.text}`)
+rabbitMQ.ack.consumer(callback, { queue: "queue-name" })
+/* -------------------------------------------------------------------------- */
+```
+
+```typescript
+/* -------------------------------------------------------------------------- */
+/*                                  Producer                                  */
+/* -------------------------------------------------------------------------- */
+import rabbitMQ from "integrations/rabbitmq"
+rabbitMQ.ack.producer(
+  { text: "Hello, world!", id: Math.round(Math.random() * 1000) },
+  { queue: "queue-name", closeConnectionAfter: 2000 }
+)
 /* -------------------------------------------------------------------------- */
 ```
--- src/integrations/rabbitmq/classes/Ack.ts
+++ src/integrations/rabbitmq/classes/Ack.ts
@@ -3,7 +3,12 @@ import { RabbitMQ } from ".."
 class Ack {
   constructor(private superThis: RabbitMQ) {}
 
-  consumer(callback: (content: any) => Promise<any>, queue_name: string): void {
+  consumer(
+    callback: (content: any) => Promise<any>,
+    options: { queue: string; durable?: boolean; noAck?: boolean; prefetch?: number }
+  ): void {
+    const { queue, durable = true, noAck = false, prefetch = 1 } = options
+
     this.superThis.amqp.connect(this.superThis.uri, (error0, connection) => {
       if (error0) throw error0
 
@@ -13,19 +18,19 @@ class Ack {
         /**
          * @argument {durable} This makes sure the queue is declared before attempting to consume from it
          */
-        channel.assertQueue(queue_name, { durable: true })
+        channel.assertQueue(queue, { durable })
 
         /**
          * @param {prefetch} This tells RabbitMQ not to give more than one message to a worker at a time.
          * Or, in other words, don't dispatch a new message to a worker until it has processed and acknowledged the previous one.
          * Instead, it will dispatch it to the next worker that is not still busy.
          */
-        channel.prefetch(1)
+        channel.prefetch(prefetch)
 
-        console.log("Waiting for messages in %s. To exit press CTRL+C", queue_name)
+        console.log("Waiting for messages in %s. To exit press CTRL+C", queue)
 
         channel.consume(
-          queue_name,
+          queue,
           (message) => {
             if (message) {
               const content = JSON.parse(message.content.toString())
@@ -34,34 +39,34 @@ class Ack {
                 .catch((error) => console.log(error.message))
             }
           },
-          { noAck: false }
+          { noAck }
         )
       })
     })
   }
 
-  producer(message: Record<string, any> = {}, queue_name: string): void {
+  producer(message: Record<string, any>, options: { queue: string; closeConnectionAfter?: number }): void {
+    const { queue, closeConnectionAfter = 500 } = options
+
     this.superThis.amqp.connect(this.superThis.uri, (error0, connection) => {
       if (error0) throw error0
 
       connection.createChannel((error1, channel) => {
-        console.log(111)
         if (error1) throw error1
-        console.log(222)
 
         /**
          * @argument {durable} This makes sure the queue is declared before attempting to consume from it
          */
-        channel.assertQueue(queue_name, { durable: true })
+        channel.assertQueue(queue, { durable: true })
 
         /**
          * @argument {persistent} Persistent messages will be written to disk as soon as they reach the queue,
          * while transient messages will be written to disk only so that they can be evicted from memory while under memory pressure
          */
-        channel.sendToQueue(queue_name, Buffer.from(JSON.stringify(message)), { persistent: true })
+        channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)), { persistent: true })
       })
 
-      setTimeout(() => connection.close(), 500)
+      setTimeout(() => connection.close(), closeConnectionAfter)
     })
   }
 }
