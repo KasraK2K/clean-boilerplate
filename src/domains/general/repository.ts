e37b87253e9a38dfb4b1f9e7f19a7dcf831d2d81
Kasra Karami <Kasra.Karami.Work@gmail.com>
2022-09-05 10:09:51 +0430

    change domains

--- src/domains/general/repository.ts
+++ src/domains/general/repository.ts
@@ -1,11 +1,11 @@
 import Repository from "../../base/repository/Repository"
 import { IDefaultArgs } from "../../common/interfaces/general.interface"
-import external_domains from "../external_domains"
+import domains from "../domains"
 
 export class GeneralRepository extends Repository {
   public async getUserList(args: IDefaultArgs = {}): Promise<Record<string, any>> {
     return new Promise(async (resolve, reject) => {
-      const userList: Record<string, any>[] = await external_domains.userDomain
+      const userList: Record<string, any>[] = await domains.userDomain
         .getUserList()
         .then((response) => response.result)
         .catch((err) => [])
