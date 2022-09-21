a5c4e74bed7140d4a176c876dd3f36908fe8a258
Kasra Karami <Kasra.Karami.Work@gmail.com>
2022-09-21 11:51:38 +0430

    change database migration

--- src/migrations/init.sql
+++ src/migrations/init.sql
@@ -14,3 +14,18 @@ CREATE DATABASE <db_name>;
 GRANT ALL PRIVILEGES ON DATABASE <db_name> TO <db_username>;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE OR REPLACE FUNCTION public.trg_timestamp()
+  RETURNS trigger
+  LANGUAGE 'plpgsql'
+  COST 100
+  VOLATILE NOT LEAKPROOF
+  AS $BODY$
+  BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+  END;
+    $BODY$;
+
+ALTER FUNCTION public.trg_timestamp()
+  OWNER TO postgres;
--- src/migrations/users.sql
+++ src/migrations/users.sql
@@ -9,45 +9,57 @@
 --======================================================================================================
 
 
-CREATE TYPE gender AS ENUM ('MALE', 'FEMALE', 'TRANSSEXUAL', 'OTHER');
+CREATE TYPE gender AS ENUM ('MALE', 'FEMALE', 'OTHER');
 
 CREATE SEQUENCE user_id_seq;
 
 CREATE TABLE IF NOT EXISTS users (
-  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-  no smallint NOT NULL DEFAULT nextval('user_id_seq'),
+  id SERIAL PRIMARY KEY,
   email VARCHAR (50) NOT NULL UNIQUE,
-  password VARCHAR (200) NOT NULL,
+  password VARCHAR (200) NOT NULL DEFAULT '',
 
-  phone VARCHAR (50),
-  first_name VARCHAR (50),
-  last_name VARCHAR (50),
+  contact_number VARCHAR (50) NOT NULL DEFAULT '',
+  first_name VARCHAR (50) NOT NULL DEFAULT '',
+  surname VARCHAR (50) NOT NULL DEFAULT '',
   gender gender DEFAULT 'OTHER',
 
   is_active BOOLEAN DEFAULT TRUE,
   is_verified BOOLEAN DEFAULT FALSE,
   is_admin BOOLEAN DEFAULT FALSE,
   is_super_admin BOOLEAN DEFAULT FALSE,
-  is_blocked BOOLEAN DEFAULT FALSE
+  is_blocked BOOLEAN DEFAULT FALSE,
+  is_archive BOOLEAN DEFAULT FALSE,
 
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  deleted_at TIMESTAMP,
+  business_name VARCHAR(100) NOT NULL DEFAULT '',
+  business_category VARCHAR(100) NOT NULL DEFAULT '',
+  business_size VARCHAR(100) NOT NULL DEFAULT '',
+  permission INT NOT NULL DEFAULT 0,
+  reseller_id INT NOT NULL DEFAULT 0,
+
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  archived_at TIMESTAMP
 );
 
+-- Create Update Trigger
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON "users"
+FOR EACH ROW
+EXECUTE PROCEDURE trg_timestamp();
+
 -- Create Indexes
-CREATE INDEX "users_is_admin" ON "users"("is_admin");
+CREATE INDEX "users_reseller_id" ON "users"("reseller_id");
 
 -- Password is 12345678 and hashed by bcryptjs salt 7
 INSERT INTO users 
   (
-    email, password, phone,
-    first_name, last_name, gender, is_active, is_verified, is_admin, is_super_admin, is_blocked
+    email, password, contact_number,
+    first_name, surname, gender, is_verified, is_admin, is_super_admin
   )
 VALUES
   (
-    'kasra.karami.kk@gmail.com', '$2a$07$r66gkFrxBP5L5/XSd4No4eY.Z/UGu.56F/neHhsLjAwydlPvUnocO', '09123456789',
-    'Kasra', 'Karami', 'MALE', TRUE, TRUE, TRUE, TRUE, FALSE
+    'a@a.com', '$2a$07$r66gkFrxBP5L5/XSd4No4eY.Z/UGu.56F/neHhsLjAwydlPvUnocO', '09123456789',
+    'Kasra', 'Karami', 'MALE', TRUE, TRUE, TRUE
   );
 
 SELECT * FROM users;
