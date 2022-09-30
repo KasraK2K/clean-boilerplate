139b867a2bda655879b56996b55f51922f3949a9
Kasra Karami <Kasra.Karami.Work@gmail.com>
2022-09-30 16:53:55 +0330

    git fix

--- src/base/Service.ts
+++ src/base/Service.ts
@@ -6,23 +6,23 @@ import tokenHelper from "../common/helpers/token.helper"
 import { TokenType } from "../common/enums/general.enum"
 
 class Service {
-  protected createToken(entity: Record<string, any>): {
+  protected createToken(admin: Record<string, any>): {
     token: string
     expire_token_in: string
     refresh_token: string
     expire_refresh_token_in: string
-    entity: Record<string, any>
+    admin: Record<string, any>
   } {
     const expire_token_in = "2 days"
     const expire_refresh_token_in = "4 days"
 
-    const token = tokenHelper.sign({ id: entity.id, type: TokenType.TOKEN }, { expiresIn: expire_token_in })
+    const token = tokenHelper.sign({ id: admin.id, type: TokenType.TOKEN }, { expiresIn: expire_token_in })
     const refresh_token = tokenHelper.sign(
-      { id: entity.id, type: TokenType.REFRESH },
+      { id: admin.id, type: TokenType.REFRESH },
       { expiresIn: expire_refresh_token_in }
     )
 
-    return { token, expire_token_in, refresh_token, expire_refresh_token_in, entity: _.omit(entity, ["password"]) }
+    return { token, expire_token_in, refresh_token, expire_refresh_token_in, admin: _.omit(admin, ["password"]) }
   }
 }
 
